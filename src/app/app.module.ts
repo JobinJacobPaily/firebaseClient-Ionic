e757f927a0ef319f91143760602128647f623d16
Jobin Jacob Paily <jobinjacobpaily@gmail.com>
2022-04-16 22:20:28 +0530

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FCM } from '@ionic-native/fcm/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    FCM,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FCM } from '@ionic-native/fcm/ngx';
+import { Platform } from '@ionic/angular';
+import { ToastService } from '../services/toast.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private fcm :FCM,
+    public platform:Platform,
+    private toastService: ToastService
+
+  ) {
+   this.platform.ready().then(() => {
+     toastService.presentToast("platform ready")
+     this.fcm.onNotification().subscribe(res => {
+       if(res.wasTapped) {
+         console.log("Notification received in background");
+           toastService.presentToast("received in background");
+       } else {
+         console.log("Notification received in forground");
+         toastService.presentToast("received in forground");
+       }
+     })
+   })
+  }
 
 }
